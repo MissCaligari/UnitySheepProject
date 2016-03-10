8b1c9521db19c97afbbf019c5442f7540c22ed48
MissCaligari <noellievelez@gmail.com>
2016-03-10 10:20:06 +0100

    Project files

---
+++ SheepProject/Assets/Scripts/SheepWaypoints.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SheepWaypoints : MonoBehaviour
+{
+    public GameObject[] myPos;
+    public bool isPatrolling = true;
+
+    private int myNextPos;
+    private GameObject player;
+    public NavMeshAgent navigationAgent;
+
+    void Start ()
+    {
+        navigationAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	void Update ()
+    {
+        if (isPatrolling)
+            Partol();
+        else
+            navigationAgent.SetDestination(player.transform.position); ;
+    }
+    void Partol()
+    {
+        //navigationAgent.stoppingDistance = 0.0f;
+        //navigationAgent.speed = 1.5f;
+        //navigationAgent.acceleration = 3.0f;
+
+        if (!navigationAgent.pathPending)
+        {
+            if (navigationAgent.remainingDistance <= navigationAgent.stoppingDistance)
+            {
+                if (!navigationAgent.hasPath || navigationAgent.velocity.sqrMagnitude == 0f)
+                {
+                    myNextPos = Random.Range(0, myPos.Length);
+                    navigationAgent.SetDestination(myPos[myNextPos].transform.position);
+                }
+            }
+        }
+    }
+}
---
+++ SheepProject/Assets/Scripts/Triggers.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Triggers : MonoBehaviour
+{
+    public Color gizmoColor = Color.yellow;
+    public GameObject sheep;
+    
+    private GameObject cam;
+
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            sheep.GetComponent<SheepWaypoints>().isPatrolling = false;
+            //sheep.GetComponent<NavMeshAgent>().SetDestination(other.transform.position);
+            if (cam.GetComponent<CameraFilterPack_Color_Chromatic_Aberration>().enabled == true)
+                cam.GetComponent<CameraFilterPack_Colors_HUE_Rotate>().enabled = true;
+            if (cam.GetComponent<CameraFilterPack_TV_Artefact>().enabled == true)
+                cam.GetComponent<CameraFilterPack_Color_Chromatic_Aberration>().enabled = true;
+            if (cam.GetComponent<CameraFilterPack_Distortion_Dream>().enabled == true)
+                cam.GetComponent<CameraFilterPack_TV_Artefact>().enabled = true;
+            cam.GetComponent<CameraFilterPack_Distortion_Dream>().enabled = true;
+            Destroy(this.gameObject);
+        }
+    }
+
+        void OnDrawGizmos()
+    {
+        Color giz = gizmoColor;
+        giz.a = 0.5f;
+        Gizmos.color = giz;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+}
